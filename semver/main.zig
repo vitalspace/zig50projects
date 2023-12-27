da94934ec4b56b4e3bac973ccb73ab00792ed9bd
vitalspace <myglobalsounds@gmail.com>
2023-12-27 16:30:41 -0600

    updated

--- semver/main.zig
+++ semver/main.zig
@@ -1,9 +1,59 @@
 const std = @import("std");
 
 const Semver = struct {
-    pub fn isValidVersion(this: @This(), version: []const u8) ![]const u8 {
+    fn checkDots(this: @This(), dots: *i32, isPreRelease: bool, segmentLength: *i32, segmentStartsWithZero: *bool, prevCharWasDot: *bool) !void {
         _ = this;
+        if (prevCharWasDot.* or (!isPreRelease and dots.* >= 2) or segmentLength.* > 15 or (segmentStartsWithZero.* and segmentLength.* > 1)) {
+            return error.InvalidVersionFormat;
+        }
+        if (!isPreRelease) {
+            dots.* += 1;
+        }
+        prevCharWasDot.* = true;
+        segmentLength.* = 0;
+        segmentStartsWithZero.* = false;
+    }
 
+    fn checkDash(this: @This(), dots: *i32, isPreRelease: *bool, segmentLength: *i32, segmentStartsWithZero: *bool, prevCharWasDot: *bool) !void {
+        _ = this;
+        if (dots.* != 2 or isPreRelease.* or segmentLength.* > 15 or (segmentStartsWithZero.* and segmentLength.* > 1)) {
+            return error.InvalidVersionFormat;
+        }
+        isPreRelease.* = true;
+        prevCharWasDot.* = false;
+        segmentLength.* = 0;
+        segmentStartsWithZero.* = false;
+    }
+
+    fn checkPlusSign(this: @This(), index: usize, buildMetadataStart: *?usize) void {
+        _ = this;
+        buildMetadataStart.* = index;
+    }
+
+    fn checkZeroStart(this: @This(), value: i32, prevCharWasDot: *bool, segmentLength: *i32, segmentStartsWithZero: *bool) !void {
+        _ = this;
+        if (value == @as(i32, '0') and segmentLength.* == 0) {
+            segmentStartsWithZero.* = true;
+        }
+        prevCharWasDot.* = false;
+        segmentLength.* += 1;
+    }
+
+    fn checkAlpha(this: @This(), prevCharWasDot: *bool, isPreRelease: bool, segmentLength: *i32) !void {
+        _ = this;
+        if (!isPreRelease) {
+            return error.InvalidVersionFormat;
+        }
+        prevCharWasDot.* = false;
+        segmentLength.* += 1;
+    }
+
+    fn checkInvalidCharacters(this: @This()) !void {
+        _ = this;
+        return error.InvalidCharacter;
+    }
+
+    pub fn isValidVersion(this: @This(), version: []const u8) ![]const u8 {
         var dots: i32 = 0;
         var prevCharWasDot: bool = false;
         var isPreRelease: bool = false;
@@ -14,44 +64,22 @@ const Semver = struct {
         for (version, 0..) |value, index| {
             switch (value) {
                 @as(i32, '.') => {
-                    if (prevCharWasDot or (!isPreRelease and dots >= 2) or segmentLength > 15 or (segmentStartsWithZero and segmentLength > 1)) {
-                        return error.InvalidVersionFormat;
-                    }
-                    if (!isPreRelease) {
-                        dots += 1;
-                    }
-                    prevCharWasDot = true;
-                    segmentLength = 0;
-                    segmentStartsWithZero = false;
+                    try this.checkDots(&dots, isPreRelease, &segmentLength, &segmentStartsWithZero, &prevCharWasDot);
                 },
                 @as(i32, '-') => {
-                    if (dots != 2 or isPreRelease or segmentLength > 15 or (segmentStartsWithZero and segmentLength > 1)) {
-                        return error.InvalidVersionFormat;
-                    }
-                    isPreRelease = true;
-                    prevCharWasDot = false;
-                    segmentLength = 0;
-                    segmentStartsWithZero = false;
+                    try this.checkDash(&dots, &isPreRelease, &segmentLength, &segmentStartsWithZero, &prevCharWasDot);
                 },
                 @as(i32, '+') => {
-                    buildMetadataStart = index;
+                    this.checkPlusSign(index, &buildMetadataStart);
                 },
                 @as(i32, '0')...@as(i32, '9') => {
-                    if (value == @as(i32, '0') and segmentLength == 0) {
-                        segmentStartsWithZero = true;
-                    }
-                    prevCharWasDot = false;
-                    segmentLength += 1;
+                    try this.checkZeroStart(value, &prevCharWasDot, &segmentLength, &segmentStartsWithZero);
                 },
                 @as(i32, 'a')...@as(i32, 'z') => {
-                    if (!isPreRelease) {
-                        return error.InvalidVersionFormat;
-                    }
-                    prevCharWasDot = false;
-                    segmentLength += 1;
+                    try this.checkAlpha(&prevCharWasDot, isPreRelease, &segmentLength);
                 },
                 else => {
-                    return error.InvalidCharacter;
+                    try this.checkInvalidCharacters();
                 },
             }
         }
@@ -181,7 +209,7 @@ pub fn main() !void {
     _ = allocator;
 
     const semver = Semver{};
-    const valid = try semver.isValidVersion("1.02.3");
+    const valid = try semver.isValidVersion("1.2.3-beta+exp.sha.5114f85");
 
     std.debug.print("{s}\n", .{valid});
 
@@ -201,22 +229,23 @@ test "fn isValidVersion" {
     const semver = Semver{};
     try std.testing.expectEqual(semver.isValidVersion("0.0.0"), "0.0.0");
     try std.testing.expectEqual(semver.isValidVersion("0.0.0-alpha"), "0.0.0-alpha");
+    try std.testing.expectEqual(semver.isValidVersion("0.0.0-0.3.7"), "0.0.0-0.3.7");
     try std.testing.expectEqual(semver.isValidVersion("0.0.0-alpha.1"), "0.0.0-alpha.1");
     try std.testing.expectEqual(semver.isValidVersion("1.2.3-alpha.1.2"), "1.2.3-alpha.1.2");
-    try std.testing.expectEqual(semver.isValidVersion("0.0.0-0.3.7"), "0.0.0-0.3.7");
+    try std.testing.expectEqualStrings(try semver.isValidVersion("1.2.3+20130313144700"), "1.2.3");
+    try std.testing.expectEqualStrings(try semver.isValidVersion("1.2.3-beta+exp.sha.5114f85"), "1.2.3-beta");
     try std.testing.expectEqual(semver.isValidVersion("999999999999999.999999999999999.999999999999999"), "999999999999999.999999999999999.999999999999999");
-    try std.testing.expectEqual(semver.isValidVersion("1.2.3.4"), error.InvalidVersionFormat);
     try std.testing.expectEqual(semver.isValidVersion("1.2.3*"), error.InvalidCharacter);
     try std.testing.expectEqual(semver.isValidVersion("1.2.3?"), error.InvalidCharacter);
     try std.testing.expectEqual(semver.isValidVersion("1.2.3/"), error.InvalidCharacter);
-    try std.testing.expectEqual(semver.isValidVersion("1.2.3beta"), error.InvalidVersionFormat);
-    try std.testing.expectEqual(semver.isValidVersion("1.2..3"), error.InvalidVersionFormat);
-    try std.testing.expectEqualStrings(try semver.isValidVersion("1.2.3-beta+exp.sha.5114f85"), "1.2.3-beta");
-    try std.testing.expectEqualStrings(try semver.isValidVersion("1.2.3+20130313144700"), "1.2.3");
     try std.testing.expectEqual(semver.isValidVersion(" 1.2.3"), error.InvalidCharacter);
     try std.testing.expectEqual(semver.isValidVersion("1.2.3 "), error.InvalidCharacter);
+    try std.testing.expectEqual(semver.isValidVersion("1.2.3-ß"), error.InvalidCharacter);
+    try std.testing.expectEqual(semver.isValidVersion("1.2..3"), error.InvalidVersionFormat);
     try std.testing.expectEqual(semver.isValidVersion("01.2.3"), error.InvalidVersionFormat);
     try std.testing.expectEqual(semver.isValidVersion("1.02.3"), error.InvalidVersionFormat);
     try std.testing.expectEqual(semver.isValidVersion("1.2.03"), error.InvalidVersionFormat);
-    try std.testing.expectEqual(semver.isValidVersion("1.2.3-ß"), error.InvalidCharacter);
+    try std.testing.expectEqual(semver.isValidVersion("1.2.3.4"), error.InvalidVersionFormat);
+    try std.testing.expectEqual(semver.isValidVersion("1.2.3beta"), error.InvalidVersionFormat);
+    try std.testing.expectEqual(semver.isValidVersion("9999999999999999.9999999999999999.9999999999999999"), error.InvalidVersionFormat);
 }
