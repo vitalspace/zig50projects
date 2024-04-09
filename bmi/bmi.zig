210a20f0cc3dbbdd42bb9b2fba45ada59c562136
vitalspace <myglobalsounds@gmail.com>
2024-04-08 22:27:40 -0600

    Update bmi.zig

--- bmi/bmi.zig
+++ bmi/bmi.zig
@@ -3,12 +3,11 @@ const std = @import("std");
 pub fn getPrompt(allocator: std.mem.Allocator) ![]const u8 {
     var stdin = std.io.getStdIn().reader();
     var buffer: [100]u8 = undefined;
-
-    var bytes_entered = try stdin.read(&buffer);
-    const entered = buffer[0..bytes_entered];
-    const trimmed_str = std.mem.trim(u8, entered, "\n ");
-    const str_copy = try allocator.alloc(u8, trimmed_str.len);
-    std.mem.copy(u8, str_copy, trimmed_str);
+    const bytes_entered = try stdin.read(&buffer);
+    const str = buffer[0..bytes_entered];
+    const str_trimmed = std.mem.trim(u8, str, "\n ");
+    const str_copy = try allocator.alloc(u8, str_trimmed.len);
+    std.mem.copyForwards(u8, str_copy, str_trimmed);
     return str_copy;
 }
 
@@ -16,16 +15,16 @@ pub fn main() !void {
     const allocator = std.heap.page_allocator;
 
     std.debug.print("Type weight: ", .{});
-    const weight_str = try getPrompt(allocator);
-    defer allocator.free(weight_str);
-    const weight = try std.fmt.parseFloat(f32, weight_str);
+    const str_weight = try getPrompt(allocator);
+    defer allocator.free(str_weight);
+    const weight = try std.fmt.parseFloat(f32, str_weight);
 
     std.debug.print("Type height: ", .{});
-    const height_str = try getPrompt(allocator);
-    defer allocator.free(height_str);
-    const height = try std.fmt.parseFloat(f32, height_str);
+    const str_height = try getPrompt(allocator);
+    defer allocator.free(str_height);
+    const height = try std.fmt.parseFloat(f32, str_height);
 
     const bmi = weight / (height * height);
 
-    std.debug.print("{d}\n", .{bmi});
+    std.debug.print("Your bmi is {}\n", .{bmi});
 }
